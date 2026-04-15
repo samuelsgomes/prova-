351f6ff232c2b1b06a65a4e4fe5303b32d48e35d
samuelsgomes <samuel.s.gomes11@aluno.senai.br>
2026-04-15 10:05:30 -0300

    first commit

---
+++ aula.js
@@ -0,0 +1,91 @@
+
+
+/////////////////////////// questao 1  //////////////////////
+
+////////// programa ///////////////
+
+
+console.log(" Essa é a lista de produtos vendidos ao dia : ")
+let numeros = [5 , 3 , 8 , 2 , 6]
+for ( let i = 1; i < numeros.Length; i++){
+    let soma = ( numeros + numeros );
+
+}
+console.log("A soma desses produtos vendidos é: " + soma );
+
+
+
+
+
+
+
+
+
+
+
+/////////////////// questao 2 ////////////////////////
+
+//////////////////// programa /////////////////////
+let numerosC = [1000, 100, 200, 500]
+
+function calcularPorcentagemr(valor, porcentagem)
+{
+    for ( let i= 1; i > numerosC.Length; i++){
+    return  ( valor * porcentagem ) / 100;
+}}
+console.log(calcularPorcentagem(numerosC, 10)); 
+
+
+
+ 
+
+
+
+
+//////////////// questao 3 /////////////////////
+
+
+
+//////// programa /////////////
+
+function  pessoa()  [
+    {nome: "Maria", idade: "25", cidade: "Caxias do Sul"},
+]
+console.log(" A cliente $(nome) possui $(idade) anos, e mora atualmente em $(cidade)");
+
+
+
+
+
+//////////// questao 4 //////////////
+const pessoas = ["Ana", "Bruno", "Carlos","Daniel", "Eva"]
+
+console.log("Primeira pessoa : " ), pessoas[0],
+
+console.log("Ultima pessoa"), pessoas(pessoas [ -1 ])
+
+
+ //////////// programa ///////////
+
+
+///////////////// questao 5 ///////////////
+
+///////////////////////// questao 8 /////////// 
+function alunos = [
+
+{ nome: "Ana", nota: 8 },
+
+
+{ nome: "Bruno", nota: 5 },
+
+
+{ nome: "Carlos", nota: 7 },
+
+
+{ nome: "Daniel", nota: 4 },
+
+(nome => 7 ){
+console.log("Esses são os alunos APROVADOS")
+}
+
+]
